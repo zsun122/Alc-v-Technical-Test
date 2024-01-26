d3c2ffde1144c46af3d430f54c414a3b223e0dca
zsun365 <zsun365@gatech.edu>
2024-01-26 14:26:37 -0500

    Initial Commit

---
+++ swift test/swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_testApp.swift
+//  swift test
+//
+//  Created by karen sun on 2024/1/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
